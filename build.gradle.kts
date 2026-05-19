60702b1b29a20b50eb5ad0694a2a0e693a2c3a25
Sydney Renee <sydney@solace.ofharmony.ai>
2026-05-19 14:55:00 -0700

    build: import anyhow-kotlin 0.1.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -131,6 +131,7 @@ kotlin {
                 implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.11.0")
                 implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.8.0")
                 implementation("org.jetbrains.kotlinx:kotlinx-collections-immutable:0.4.0")
+                implementation("io.github.kotlinmania:anyhow-kotlin:0.1.1")
             }
         }
         val commonTest by getting {
